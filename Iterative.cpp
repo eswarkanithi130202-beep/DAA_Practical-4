8fa889414b9f8171e55cc94df0beb20d86f2a1f0
Eswar Kanitih <eswar.kanithi130202@marwadiuniversity.ac.in>
2026-08-13 13:57:54 +0530

    Create Iterative.cpp

---
+++ Iterative.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+long long factorialIterative(int n) {
+    long long fact = 1;
+
+    for (int i = 1; i <= n; i++) {
+        fact = fact * i;
+    }
+
+    return fact;
+}
+
+int main() {
+    int n;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << "Factorial using Iterative Method: "
+         << factorialIterative(n) << endl;
+
+    return 0;
+}
